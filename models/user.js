6cef7e6beb81c6f52fb42e2833cc8592b9b51e85
karl soldevilla <karllorence0000@gmail.com>
2026-03-03 23:21:53 +0800

    Fix: Remove invalid isEmail validator from User model

--- models/user.js
+++ models/user.js
@@ -16,7 +16,6 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING(255),
         allowNull: false,
         unique: true,
-        validate: { isEmail: true },
       },
 
       password: {
